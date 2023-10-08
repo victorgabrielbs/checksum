24d6c820c506fa033acf72a8b7776778493537aa
Victor Gabriel <victorgabrielbritodasilva@gmail.com>
2023-10-08 15:58:05 -0300

    Refactoring due to recent change
    As the logger configuration is in a different location, it was removed from here, in addition the method of checking the sum was renamed to something that makes more sense

--- src/main/java/com/kldv/PrimaryController.java
+++ src/main/java/com/kldv/PrimaryController.java
@@ -7,10 +7,8 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -47,7 +45,7 @@ public class PrimaryController {
     private Preferences userPreferences;
     private ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-    private static final Logger logger = Logger.getLogger(PrimaryController.class.getName());
+    private static final Logger logger = LoggerConfig.configureLogger();
 
     @FXML
     private void initialize() {
@@ -57,26 +55,10 @@ public class PrimaryController {
                 executorService.shutdown();
             });
         });
-        configureLogger();
+        LoggerConfig.configureLogger();
         firstRadioButton.setDisable(false);
         secondRadioButton.setDisable(false);
         userPreferences = Preferences.userNodeForPackage(getClass());
-    }
-
-    private void configureLogger() {
-        try {
-            File logFolder = new File("logs");
-
-            if (!logFolder.exists()) {
-                logFolder.mkdir();
-            }
-            FileHandler fileHandler = new FileHandler("logs/app.log", true);
-            SimpleFormatter formatter = new SimpleFormatter();
-            fileHandler.setFormatter(formatter);
-            logger.addHandler(fileHandler);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @FXML
@@ -117,7 +99,7 @@ public class PrimaryController {
 
                 executorService.submit(() -> {
                     try {
-                        String checksum = calculateChecksum(selectedFile, algorithm);
+                        String checksum = calculateSum(selectedFile, algorithm);
                         Platform.runLater(() -> {
                             resultChecksum = checksum;
                             txtResultChecksum.setText(resultChecksum);
@@ -144,7 +126,7 @@ public class PrimaryController {
         }
     }
 
-    private String calculateChecksum(File file, String algorithm)
+    private String calculateSum(File file, String algorithm)
             throws NoSuchAlgorithmException, IOException {
         MessageDigest digest = MessageDigest.getInstance(algorithm);
         try (FileInputStream fis = new FileInputStream(file)) {
@@ -155,7 +137,7 @@ public class PrimaryController {
             }
         } catch (IOException e) {
             txtResultChecksum.setText("Aconteceu algum problema, tente denovo");
-            logger.log(Level.SEVERE, "An error occurred in calculateChecksum() method", e);
+            logger.log(Level.SEVERE, "An error occurred in calculateSum() method", e);
         }
         byte[] bytes = digest.digest();
         StringBuilder sb = new StringBuilder();
