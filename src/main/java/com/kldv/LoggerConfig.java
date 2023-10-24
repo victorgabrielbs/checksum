57747f677e279be11b09dfdac8bf9295b6db0aca
Victor Gabriel <victorgabrielbritodasilva@gmail.com>
2023-10-23 21:57:44 -0300

    refactoring and configuring correctly
    refactoring the code  and changing the logger object's visibility  to correctly configure the logging.

--- src/main/java/com/kldv/LoggerConfig.java
+++ src/main/java/com/kldv/LoggerConfig.java
@@ -8,20 +8,23 @@ import java.util.logging.SimpleFormatter;
 
 public class LoggerConfig {
 
-  private static final Logger logger = Logger.getLogger(LoggerConfig.class.getName());
+  static final Logger logger = Logger.getLogger(LoggerConfig.class.getName());
 
   public static Logger configureLogger() {
     try {
+
       File logFolder = new File("logs");
 
       if (!logFolder.exists()) {
         logFolder.mkdir();
       }
+
       FileHandler fileHandler = new FileHandler("logs/app.log", true);
       SimpleFormatter formatter = new SimpleFormatter();
       fileHandler.setFormatter(formatter);
       logger.addHandler(fileHandler);
       return logger;
+
     } catch (IOException e) {
       e.printStackTrace();
       return null;
