e5fbcc3ec9b78c7b18ebd992c03aab200d307b53
Victor Gabriel <victorgabrielbritodasilva@gmail.com>
2023-10-23 21:54:06 -0300

    refactoring
    refactoring, improving clarity by removing magic numbers and correctly using the log configuration

--- src/main/java/com/kldv/Calculate.java
+++ src/main/java/com/kldv/Calculate.java
@@ -6,32 +6,35 @@ import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Calculate {
 
-  private static Logger logger = LoggerConfig.configureLogger();
+  private static final int BUFFER_SIZE = 1024;
 
   static String calculateSum(File file, String algorithm) throws NoSuchAlgorithmException, IOException {
+
     MessageDigest digest = MessageDigest.getInstance(algorithm);
 
     try (FileInputStream fis = new FileInputStream(file)) {
 
-      byte[] byteArray = new byte[1024];
+      byte[] byteArray = new byte[BUFFER_SIZE];
       int bytesCount;
+
       while ((bytesCount = fis.read(byteArray)) != -1) {
         digest.update(byteArray, 0, bytesCount);
       }
 
     } catch (IOException e) {
-      logger.log(Level.SEVERE, "An error occurred in calculateSum() method", e);
+      LoggerConfig.logger.log(Level.SEVERE, "An error occurred in calculateSum() method", e);
     }
 
     byte[] bytes = digest.digest();
     StringBuilder sb = new StringBuilder();
 
+    final int HEX_MASK = 0x100;
+
     for (byte aByte : bytes) {
-      sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+      sb.append(Integer.toString((aByte & 0xff) + HEX_MASK, 16).substring(1));
     }
 
     return sb.toString();
